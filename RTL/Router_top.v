cee372c84b147ac1c40a225a80bb44413316c9b2
saikumarvandanapu <141210383+saikumarvandanapu@users.noreply.github.com>
2024-12-05 14:40:21 +0530

    Update Router_top.v

--- RTL/Router_top.v
+++ RTL/Router_top.v
@@ -2,142 +2,44 @@ Title: Router Top Module
 Author: Saikumar
 
 
-module router_sync(clk,reset_in,detect_add,write_en_reg,read_en_0,read_en_1,read_en_2,empty_0,empty_1,empty_2,
-full_0,full_1,full_2,data_in,write_en,soft_rst_0,soft_rst_1,soft_rst_2,fifo_full,valid_out_0,valid_out_1,valid_out_2);
-input clk,reset_in,detect_add,write_en_reg,read_en_0,read_en_1,read_en_2,empty_0,empty_1,empty_2,full_0,full_1,full_2;
-input [1:0]data_in;
-output reg [2:0]write_en;
-output reg soft_rst_0,soft_rst_1,soft_rst_2;
-output reg fifo_full;
-output valid_out_0,valid_out_1,valid_out_2;
+module router_top(
+input clk,resetn,read_enb_0,read_enb_1,read_enb_2,pkt_valid,
+input [7:0]data_in,
+output valid_out_0,valid_out_1,valid_out_2,error,busy,
+output [7:0]data_out_0,data_out_1,data_out_2
+);
+wire w,w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15;
+wire [2:0]full;
+wire [2:0]write_enb;
+wire [7:0]data;
 
-reg [1:0]temp_reg; //to store the data and based on it generates the write_en;
-reg [4:0]timer_0,timer_1,timer_2; //for 30 clk pulses of each fifo;
+//fsm
+router_fsm	fsm1(.clk(clk) ,.resetn(resetn) ,.data_in(data_in[1:0]) ,.pkt_valid(pkt_valid) ,.parity_done(w1) ,.soft_reset_0(w2) ,.soft_reset_1(w3) ,.soft_reset_2(w4)
+,.fifo_full(w5) ,.low_pkt_valid(w6) ,.fifo_empty_0(w7) ,.fifo_empty_1(w8) ,.fifo_empty_2(w9) ,.detect_add(w10) ,.ld_state(w11) ,.laf_state(w12) ,.full_state(w13)
+,.write_enb_reg(w) ,.rst_int_reg(w14) ,.lfd_state(w15) ,.busy(busy));
 
-//capturing address if detect_add is 1
-always@(posedge clk)
-	begin
-		if(!reset_in)
-			temp_reg<=2'b00;
-		else if(detect_add)
-				temp_reg<=data_in;
-	end
+//synchronizer
+router_sync	sync1(.clk(clk) ,.reset_in(resetn) ,.detect_add(w10) ,.write_en_reg(w) ,.read_en_0(read_enb_0) ,.read_en_1(read_enb_1) ,.read_en_2(read_enb_2)
+ ,.empty_0(w7) ,.empty_1(w8) ,.empty_2(w9) ,.full_0(full[0]) ,.full_1(full[1]) ,.full_2(full[2]) ,.data_in(data_in[1:0]) ,.write_en(write_enb),
+ .soft_rst_0(w2) ,.soft_rst_1(w3) ,.soft_rst_2(w4) ,.fifo_full(w5) ,.valid_out_0(valid_out_0) ,.valid_out_1(valid_out_1) ,.valid_out_2(valid_out_2));
 
-//write enable logic
-always@(*)
-	begin
-		if(write_en_reg)
-			begin
-				case(temp_reg)
-				2'b00:write_en=3'b001;
-				2'b01:write_en=3'b010;
-				2'b10:write_en=3'b100;
-				default:write_en=3'b000;
-				endcase
-			end
-		else
-			write_en=3'b000;
-	end
-	
-//fifo_full logic
-always@(*)
-	begin
-		case(temp_reg)
-		2'b00:fifo_full=full_0;
-		2'b01:fifo_full=full_1;
-		2'b10:fifo_full=full_2;
-		default:fifo_full=1'b0;
-		endcase
-	end
+//register
+ router_reg	register(.clk(clk) ,.resetn(resetn) ,.pkt_valid(pkt_valid) ,.data_in(data_in) ,.fifo_full(w5) ,.rst_in_reg(w14) ,
+ .detect_add(w10) ,.ld_state(w11) ,.laf_state(w12) ,.full_state(w13) ,.lfd_state(w15) ,.parity_done(w1) ,.low_pkt_valid(w6) ,.err(error) ,.d_out(data));
+ 
+ //fifo_1
+ router_fifo	fifo1(.clk(clk) ,.resetn(resetn) ,.soft_reset(w2) ,.write_enb(write_enb[0]) ,.read_enb(read_enb_0) ,.lfd_state(w15) ,.data_in(data) ,.data_out(data_out_0) ,
+ .full(full[0]) ,.empty(w7));
+ 
+ //fifo_2
+ router_fifo	fifo2(.clk(clk) ,.resetn(resetn) ,.soft_reset(w3) ,.write_enb(write_enb[1]) ,.read_enb(read_enb_1) ,.lfd_state(w15) ,.data_in(data) ,.data_out(data_out_1) ,
+ .full(full[1]) ,.empty(w8));
+ 
+ //fifo_3
+ router_fifo	fifo3(.clk(clk) ,.resetn(resetn) ,.soft_reset(w4) ,.write_enb(write_enb[2]) ,.read_enb(read_enb_2) ,.lfd_state(w15) ,.data_in(data) ,.data_out(data_out_2) ,
+ .full(full[2]) ,.empty(w9));
+ 
+ endmodule
 
-//soft reset_0 logic
-always@(posedge clk)
-	begin
-		soft_rst_0<=1'b0;
-		timer_0<=5'd0;
-		if(!reset_in)
-			begin
-				soft_rst_0<=1'b0;
-				timer_0<=5'd0;
-			end
-		else if(valid_out_0)
-				begin
-					if(!read_en_0)
-						begin
-							if(timer_0==5'd29)
-							begin
-								soft_rst_0<=1'b1;
-								timer_0<=5'd0;
-							end
-								else
-									begin
-										soft_rst_0<=1'b0;
-										timer_0<=timer_0+5'd1;
-									end
-						end
-				end
-	end
-	
-//soft reset_1 logic
-always@(posedge clk)
-	begin
-		soft_rst_1<=1'b0;
-		timer_1<=5'd0;
-		if(!reset_in)
-			begin
-				soft_rst_1<=1'b0;
-				timer_1<=5'd0;
-			end
-		else if(valid_out_1)
-				begin
-					if(!read_en_1)
-						begin
-							if(timer_1==5'd29)
-							begin
-								soft_rst_1<=1'b1;
-								timer_1<=5'd0;
-							end
-								else
-									begin
-										soft_rst_1<=1'b0;
-										timer_1<=timer_1+5'd1;
-									end
-						end
-				end
-	end
-	
-//soft reset_2 logic
-always@(posedge clk)
-	begin
-		soft_rst_2<=1'b0;
-		timer_2<=5'd0;
-		if(!reset_in)
-			begin
-				soft_rst_2<=1'b0;
-				timer_2<=5'd0;
-			end
-		else if(valid_out_2)
-				begin
-					if(!read_en_2)
-						begin
-							if(timer_2==5'd29)
-							begin
-								soft_rst_2<=1'b1;
-								timer_2<=5'd0;
-							end
-								else
-									begin
-										soft_rst_2<=1'b0;
-										timer_2<=timer_2+5'd1;
-									end
-						end
-				end
-	end
-
-//valid_out value is 1 only if empty is 0
-assign valid_out_0=~empty_0;
-assign valid_out_1=~empty_1;
-assign valid_out_2=~empty_2;
-
-endmodule						
+						
 				
